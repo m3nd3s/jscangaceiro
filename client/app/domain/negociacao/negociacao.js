0704a06c1d4b6e4afd4f8ce0ba66df91249e9bdf
Almir Mendes <m3nd3s@gmail.com>
2020-11-20 09:00:21 -0300

    Add getters

--- client/app/domain/negociacao/negociacao.js
+++ client/app/domain/negociacao/negociacao.js
@@ -1,7 +1,23 @@
 class Negociacao {
-  constructor() {
-    this.data = new Date();
-    this.quantidade = 1;
-    this.value = 0.0;
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+
+  get volume() {
+    return this._quantidade * this._valor;
+  }
+
+  get quantidade() {
+    return this._quantidade;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  get valor() {
+    return this._valor;
   }
 }
